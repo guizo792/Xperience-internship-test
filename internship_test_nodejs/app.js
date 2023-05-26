8951fb8f5772fff4aace13d872a6c54bd74db531
Guizoul <guizoul.abdellah@ine.inpt.ma>
2023-05-26 13:27:07 +0100

    added not found routes middelware

--- internship_test_nodejs/app.js
+++ internship_test_nodejs/app.js
@@ -5,21 +5,20 @@ const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
 
-// 1) GLOBAL MIDDLWARES
 // Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-// 3) ROUTES
-app.use("/api/v1/reviews", reviewRouter);
+// Routes
+app.use("/api/reviews", reviewRouter);
 
 // Any other route other than /reviews
-// app.all("*", (req, res, next) => {
-//   // GET /me? 200 153.699 ms - 3669
-//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-// });
-
-// // app.use(globalErrorHandler);
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
